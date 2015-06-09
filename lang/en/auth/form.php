a4061647fb93f170317a867380b67ff8cd87dd53
Ben Johnson <bjohnson@medicalmutual.com>
2015-06-09 12:37:15 -0400

    Added translations for form field data that deviates from stock implementation.

---
+++ lang/en/auth/form.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+
+	'username'                       => 'Username',
+	'username_placeholder'           => 'jdoe',
+	'username_help'                  => 'Type your username (this is the same name that you use to log onto your work computer).',
+	'username_error'                 => 'A valid username is required.',
+
+];
---
+++ lang/en/auth/message.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+	// General messages
+	'user_not_found'      => 'Incorrect username or password.',
+	'invalid_username'       => 'Username is either invalid or was not found.',
+
+	// Error messages
+	'error' => [
+		'reset-password'         => 'Unable to reset your password, please make sure you are using a registered username.',
+	],
+
+];
