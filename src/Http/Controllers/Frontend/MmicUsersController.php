64885640347aba93dfe64887bf3939d07171e244
Ben Johnson <bjohnson@medicalmutual.com>
2015-08-19 09:05:35 -0400

    Working to fix issue with $currentUser not being set (it's normally displayed in the header).

--- src/Http/Controllers/Frontend/MmicUsersController.php
+++ src/Http/Controllers/Frontend/MmicUsersController.php
@@ -2,13 +2,21 @@
 
 
 use Platform\Users\Controllers\Frontend\UsersController;
+use Platform\Users\Repositories\UserRepositoryInterface;
 
 class MmicUsersController extends UsersController {
 
 /**
 * {@inheritDoc}
 */
+public function __construct(UserRepositoryInterface $userRepositoryInterface)
+{
+	parent::__construct($userRepositoryInterface);
+}
 
+/**
+* {@inheritDoc}
+*/
 public function login()
 {
 	$connections = $this->users->auth()->getSocialConnections();
