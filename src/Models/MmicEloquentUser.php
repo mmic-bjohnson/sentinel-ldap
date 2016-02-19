c2e570b58dc7f93bee41a63a6cd47cb7f29948b0
Ben Johnson <bjohnson@medicalmutual.com>
2016-02-19 11:01:14 -0500

    Added the SoftDelete trait to our custom user model so that Platform users are soft-deleted (instead of hard-deleted) when deleted via the Platform Admin GUI.

--- src/Models/MmicEloquentUser.php
+++ src/Models/MmicEloquentUser.php
@@ -1,9 +1,13 @@
 <?php namespace Mmic\SentinelLdap\Models;
 
 
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 use Platform\Users\Models\User;
 
 class MmicEloquentUser extends User {
+
+use SoftDeletes;
 
 protected $loginNames = ['samAccountName'];
 
