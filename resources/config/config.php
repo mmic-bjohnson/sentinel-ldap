38d043a77b93bf7a9f7e74e2b3008e16fa45d601
Ben Johnson <bjohnson@medicalmutual.com>
2020-05-08 09:33:51 -0400

    Update config file to include variables that should have been defined here in the first place

--- resources/config/config.php
+++ resources/config/config.php
@@ -1,7 +1,9 @@
 <?php
 
 return [
-
+	
+	'disable_ldap_check' => env('DISABLE_LDAP_CHECK', false),
+	'user_model' => 'Mmic\Intranet\Models\User\User',
 	'authentication_domain' => '@medicalmutual.com',
 	'default_sentinel_role_slugs' => ['staff', 'ist-user'],
 
